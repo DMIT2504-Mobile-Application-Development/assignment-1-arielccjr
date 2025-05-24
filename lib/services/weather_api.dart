a4bd906f12ddca8c35e805047496e9a107e3b48d
ARIEL JR. <arielccjunior@protonmail.com>
2025-05-24 00:01:45 -0600

    Writing try-catch block...

--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -13,9 +13,20 @@ const String currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/w
 
   // - A `getWeatherForCity` function must be exported. The function accepts the `city` as a _named parameter_ and will return the `dynamic` JSON data object received from the OpenWeather API endpoint `https://api.openweathermap.org/data/2.5/weather?units=metric&q={city name}&appid={API key}` (use the `currentWeatherEndpoint` from a previous step above to create the full URL).
 Future<dynamic> getWeatherForCity({required String city}) async {
-  final String url = '$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey';
-}
-    // - If the response status code is anything other than 200, throw an `Exception` with a message in the following format: `There was a problem with the request: status <status code> received`.
+  final url = Uri.parse('$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey');
 
+  try {
+    final response = await http.get(url); 
+    
+    // - If the response status code is anything other than 200, throw an `Exception` with a message in the following format: `There was a problem with the request: status <status code> received`.
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body); // Decode the JSON response body into a Dart dynamic object (Map or List).
+    } else {
+      throw Exception('There was a problem with the request: status ${response.statusCode} received.');
+    }
 
     // - If an exception occurs when accessing the endpoint, throw an `Exception` with a message in the following format: `There was a problem with the request: <original exception message>`.
+  } catch (e) {
+    throw Exception('There was a problem with the request: ${e.toString()}');
+  }
+}
