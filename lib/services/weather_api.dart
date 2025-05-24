9d534f654763802fde206a5803c8d5fc3ba5edb1
ARIEL JR. <arielccjunior@protonmail.com>
2025-05-23 23:08:33 -0600

    Copying the same code from the workbook /week_02_sync_and_json/lib/web_fetch.dart...

--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -1,6 +1,9 @@
 // 1. Create a Weather Service API
 // - Create a `services/` directory in the `lib/` directory.
 // - Create a `weather_api.dart` file in the `services/` directory, this is where you will write your API functions.
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 // - The following requirements must be met:
   // - The OpenWeather API key must be exported as a constant named `weatherApiKey`
 const String weatherApiKey = 'ibe9c3b7e8fb0235f67e628b885cb47e7';
@@ -9,8 +12,11 @@ const String weatherApiKey = 'ibe9c3b7e8fb0235f67e628b885cb47e7';
 const String currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
 
   // - A `getWeatherForCity` function must be exported. The function accepts the `city` as a _named parameter_ and will return the `dynamic` JSON data object received from the OpenWeather API endpoint `https://api.openweathermap.org/data/2.5/weather?units=metric&q={city name}&appid={API key}` (use the `currentWeatherEndpoint` from a previous step above to create the full URL).
-
-
+Future<dynamic> getWeatherForCity(String endpoint) async {
+  final url = Uri.parse(endpoint);
+  final response = await http.get(url);
+  return jsonDecode(reponse.body);
+}
     // - If the response status code is anything other than 200, throw an `Exception` with a message in the following format: `There was a problem with the request: status <status code> received`.
 
 
